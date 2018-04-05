41e52f6706b4df57ea5a894c2ac4615d29d38980
Alexey <alex.v.timofeev@gmail.com>
2018-04-06 00:06:15 +0500

    init console app

---
+++ src/NhFirebirdConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhFirebirdConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO https://github.com/FluentNHibernate/fluent-nhibernate/wiki/Getting-started#yourfirstproject
+        }
+    }
+}
